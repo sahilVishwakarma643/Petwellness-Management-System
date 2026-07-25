e29ad9aed9648db725e7803bf160928608999b79
Sahil Vishwakarma <vishwakarmasahil643@gmail.com>
2026-07-26 00:04:49 +0530

    aded constructor with cause in parameter

--- src/main/java/com/petcare/petwellness/Exceptions/CustomException/ServiceUnavailableException.java
+++ src/main/java/com/petcare/petwellness/Exceptions/CustomException/ServiceUnavailableException.java
@@ -4,4 +4,7 @@ public class ServiceUnavailableException extends RuntimeException {
     public ServiceUnavailableException(String message) {
         super(message);
     }
+    public ServiceUnavailableException(String message, Throwable cause){
+        super(message,cause);
+    }
 }
